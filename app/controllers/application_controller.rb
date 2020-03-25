af32dc8f5456e5d035d2bedabe372e8ab7e685a6
Varun Kumaar <teralad@gmail.com>
2020-03-25 23:56:21 +0530

    changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,2 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
-
-
-  def authenticate_and_set_user
-    token = request.headers['HTTP_AUTHORIZATION']&.split(' ')&.last
-    Rails.logger.info "Header auth is #{request.headers['HTTP_AUTHORIZATION']}"
-    if token&.count('.') == 2
-      decoded_token, = JWT.decode token, AUTH_PUBLIC_KEY, true, algorithm: 'RS256'
-      @user_id, @role_id, @jti = decoded_token.values_at('user_id', 'role_id', 'jti')
-    end
-  rescue StandardError => e
-    raise "Error with token sent. #{e.message}"
-  end
-
-  def check_user_login_status
-    unless Cache.core.get("jwt:#{@user_id}:#{@jti}").present?
-      render json: {error: 'User not logged in'}, status: 401
-    end
-  end
-
 end
