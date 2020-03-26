110ed03566b22d19b6add599a0545472974b0096
Varun Kumaar <teralad@gmail.com>
2020-03-26 13:25:19 +0530

    set status and add enum status in posts for moderation

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,14 @@ class Post < ApplicationRecord
   has_many :votes, :as => :voteable
   after_commit :create_or_update_slug, if: :bad_slug
 
+  enum status: { created: 0, published: 1, archived: 2, deleted: 3 }, _suffix: true
+
+  before_create :set_status
+
+  def set_status
+    self.status = 'created'
+  end
+
   def bad_slug
     self.slug != new_slug
   end
