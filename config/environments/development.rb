9b67b443aefc51e65264c77b01574d5b6babcbcc
Varun Kumaar <teralad@gmail.com>
2020-03-27 10:49:33 +0530

    reverting stg and prod env changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,29 +7,25 @@ Rails.application.configure do
   config.cache_classes = false
 
   # Do not eager load code on boot.
-  config.eager_load = false
+  config.eager_load = true
 
   # Show full error reports.
   config.consider_all_requests_local = true
 
+
   # Enable/disable caching. By default caching is disabled.
-  # Run rails dev:cache to toggle caching.
-  if Rails.root.join('tmp', 'caching-dev.txt').exist?
+  if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
-    config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+      'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
     }
   else
     config.action_controller.perform_caching = false
 
     config.cache_store = :null_store
   end
-
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -41,22 +37,41 @@ Rails.application.configure do
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
+  logger           = ActiveSupport::Logger.new(STDOUT)
+  logger.formatter = config.log_formatter
+  config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  # Sidekiq::Logging.logger = ActiveSupport::Logger.new(STDOUT)
 
-  # Highlight code that triggered database queries in logs.
-  config.active_record.verbose_query_logs = true
-
-  # Debug mode disables concatenation and preprocessing of assets.
-  # This option may cause significant delays in view rendering with a large
-  # number of complex assets.
-  config.assets.debug = true
-
-  # Suppress logger output for asset requests.
-  config.assets.quiet = true
-
-  # Raises error for missing translations.
+  # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
+#   # Debug mode disables concatenation and preprocessing of assets.
+#   # This option may cause significant delays in view rendering with a large
+#   # number of complex assets.
+#   config.assets.debug = true
+#   config.assets.quiet = true
+#   config.log_level = :debug
+#   config.log_tags = [ :request_id ]
+#   config.active_support.deprecation = :notify
+#   config.i18n.fallbacks = true
+#   config.log_formatter = ::Logger::Formatter.new
+#   if ENV["RAILS_LOG_TO_STDOUT"].present?
+#     logger           = ActiveSupport::Logger.new(STDOUT)
+#     logger.formatter = config.log_formatter
+#     config.logger    = ActiveSupport::TaggedLogging.new(logger)
+#   end
+
+#   # Suppress logger output for asset requests.
+#   config.assets.quiet = true
+
+#   # Raises error for missing translations.
+#   # config.action_view.raise_on_missing_translations = true
+
+#   # Use an evented file watcher to asynchronously detect changes in source code,
+#   # routes, locales, etc. This feature depends on the listen gem.
+#   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+# end
